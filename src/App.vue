d9b60b5e63426dbb5b0ece42ceb7647766192932
Nathan Bubna <nathan@esha.com>
2017-08-30 19:10:21 -0700

    stub out some nav

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="app">
+    <nav>
+      <h2><router-link to="/">Genesis API Demo</router-link></h2>
+      <ul>
+        <li><router-link to="/slugway">Slugway (Interactive Data Demo)</router-link></li>
+        <li><router-link to="/mabisco">Mabisco (Fixed Data Demo)</router-link></li>
+        <li><router-link to="/api">API Docs</router-link></li>
+        <li><router-link to="/dev">Demo Docs</router-link></li>
+      </ul>
+    </nav>
     <router-view></router-view>
-    <img src="./assets/logo.png">
   </div>
 </template>
 
@@ -19,5 +27,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 10px;
+}
+nav ul {
+  list-style-type: none;
+}
+nav a {
+  text-decoration: none;
 }
 </style>
