097352b470b5b35ef58c1429e4a480ef38f7acd0
Nathan Bubna <nathan@esha.com>
2017-08-30 19:12:30 -0700

    more tslint and a home page stub

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+      Hello, world.
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'devdoc',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  font-size: 2em;
+}
+</style>
