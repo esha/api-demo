8819b5a4166fd7525647aeed010c828acf7c76ef
Nathan Bubna <nathan@esha.com>
2017-10-17 08:19:57 -0700

    semi-colons and proper tabs

--- src/main.ts
+++ src/main.ts
@@ -3,33 +3,36 @@
 // 1. use next line to activate CUSTOM STYLE (./src/themes)
 // require(`./themes/app.${__THEME}.styl`)
 // 2. or, use next line to activate DEFAULT QUASAR STYLE
-require(`quasar/dist/quasar.${__THEME}.css`)
+require(`quasar/dist/quasar.${__THEME}.css`);
 // ==============================
 
 // Uncomment the following lines if you need IE11/Edge support
 // require(`quasar/dist/quasar.ie`)
 // require(`quasar/dist/quasar.ie.${__THEME}.css`)
 
-import Vue from 'vue'
-import Quasar from 'quasar'
-import router from './router'
+import Vue from 'vue';
+import Quasar from 'quasar';
+import router from './router';
 
-Vue.config.productionTip = false
-Vue.use(Quasar) // Install Quasar Framework
+Vue.config.productionTip = false;
+Vue.use(Quasar); // Install Quasar Framework
 
 if (__THEME === 'mat') {
-  require('quasar-extras/roboto-font')
+    require('quasar-extras/roboto-font');
 }
-import 'quasar-extras/material-icons'
+import 'quasar-extras/material-icons';
 // import 'quasar-extras/ionicons'
 // import 'quasar-extras/fontawesome'
 // import 'quasar-extras/animate'
 
 Quasar.start(() => {
-  /* eslint-disable no-new */
-  new Vue({
-    el: '#q-app',
-    router,
-    render: h => h(require('./App'))
-  })
-})
+    /* eslint-disable no-new */
+    new Vue({
+        el: '#q-app',
+        router,
+        render: function(h) {
+            window.console.log('wtf', h);
+            return h(require('./App'));
+        }
+    });
+});
